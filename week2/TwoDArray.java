3c431d1b27cf2b910498f06a3cc1b89aacacadeb
chandrashekhar2006 <chandusagar.2006@gmail.com>
2026-02-18 12:32:40 +0530

    Add files via upload

---
+++ week2/TwoDArray.java
@@ -0,0 +1,31 @@
+//Demonstrate a two d array.
+class TwoDArray
+{
+	public static void main(String[] args)
+   {
+		int twoD[][]=new int[4][5];
+        int i,j, k=0;
+
+//Fill the array
+        for(i=0;i<4;i++)
+       {
+			for(j=0;j<5;j++)
+            {
+				twoD[i][j]=k;
+                k++;
+            }
+	    }
+	    for(i=0;i<4;i++)
+	   {
+			for(j=0;j<5;j++)
+		   {
+				System.out.print(twoD[i][j] + " ");
+		   }
+		   System.out.println();
+	   }
+   }
+}
+		
+	
+
+		
